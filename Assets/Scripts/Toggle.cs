fcc642632263eb513c698fbfa3f000fbf5569f87
Ashley Klahm <klahmashley@gmail.com>
2025-10-23 17:25:32 -0400

    --

---
+++ Assets/Scripts/Toggle.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering.PostProcessing;
+
+public class Toggle : MonoBehaviour
+{
+    public GameObject Hair;
+  
+    public Material Base;
+    public Material Diffuse;
+    public Material DiffuseAmbient;
+    public Material Specular;
+    public Material ToonShader;
+
+    public GameObject Lut1;
+
+
+    void Start()
+    {
+        // _Material = takenfrom.GetComponent<MeshRenderer>().material;
+        Hair.GetComponent<SkinnedMeshRenderer>().material = Base;
+
+        Lut1.SetActive(false);
+ 
+    }
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Hair.GetComponent<SkinnedMeshRenderer>().material = Base;
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Hair.GetComponent<SkinnedMeshRenderer>().material = Diffuse;
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Hair.GetComponent<SkinnedMeshRenderer>().material = DiffuseAmbient;
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Hair.GetComponent<SkinnedMeshRenderer>().material = Specular;
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            Hair.GetComponent<SkinnedMeshRenderer>().material = ToonShader;
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            Lut1.SetActive(true);
+        }
+    }
+}
